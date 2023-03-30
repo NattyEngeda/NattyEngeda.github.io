2e94aa414c1f0b12aa976d9c69db23ed151c019b
Naatty123 <nattynengeda@gmail.com>
2023-03-30 23:18:39 +0300

    GIt Push Start

--- src/layouts/Header.tsx
+++ src/layouts/Header.tsx
@@ -1,5 +1,11 @@
 import React from "react";
 
 export default function Header() {
-  return <div>Header</div>;
+  return (
+    <header className="h-20 normshad flex flex-row gap-20 items-center justify-between px-10">
+      <div>
+        <h1 className="text-2xl font-semibold">Natty Engeda</h1>
+      </div>
+    </header>
+  );
 }
