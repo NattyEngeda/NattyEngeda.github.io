bf680392065c70cef6ab1e1a31e56ec1ff3fea7c
NattyEngeda <nattynengeda@gmail.com>
2023-09-08 17:32:44 +0300

    Commit

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -6,23 +6,28 @@ export default function Index() {
   return (
     <>
       <div className="w-full min-h-screen flex flex-col gap-5 py-10">
-        <div className="container px-10 grid grid-cols-2 gap-10">
 
+        {/* First Section */}
+        <div className="container px-5 md:px-10 grid grid-colrs-1 md:grid-cols-2 gap-10">
           {/* Name */}
           <div
-            className="text-white text-4xl flex flex-col gap-10 items-start justify-center border-white border h-96 px-5 font-VT323 font-bold">
-            <TypeWriterText text='Hello There!,' pauseFor={2900} />
-            <p>I'm {" "}
-              <span className="text-primary-500 link-underline">Natty.Engeda</span></p>
-
+            className="text-white text-4xl flex flex-col gap-10 items-start justify-center border-white border h-96 px-5 font-VT323 font-bold shadow-xl shadow-gray-700">
+            {/* <TypeWriterText text='Hello There!,' pauseFor={3900} /> */}
+            {/* <TypeWriterText text='<p>I am <span style="color:#34568b" className="text-primary-500 link-underline">Natty.Engeda</span></p>' pauseFor={3900} /> */}
+            <p>Hello There!, </p>
+            <p>I am <span className="text-primary-500 link-underline">Natty.Engeda</span></p>
           </div>
 
           {/* Some Image */}
           <div>
-
           </div>
+        </div>
+
+        {/* Second Sections */}
+        <div className="container px-10">
 
         </div>
+
       </div>
     </>
   );
